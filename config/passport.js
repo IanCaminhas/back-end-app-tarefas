314a4c02864e5fcea8dad3d60a8c33d9b399ac53
IanCaminhas <iancaminhas25@hotmail.com>
2020-12-25 15:00:38 -0300

    API finalizada

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const {authSecret} = require('../.env')
 const passport = require('passport')
-const passportJwt = require('password-jwt')
+const passportJwt = require('passport-jwt')
 const {Strategy, ExtractJwt} = passportJwt
 
 module.exports = app =>{
